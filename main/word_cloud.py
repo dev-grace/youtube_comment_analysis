76a8ed2d543f58ccf7df16d88da41570d9e42f2f
dev-grace <qkrdmsh24@naver.com>
2022-05-26 17:39:27 +0900

    Add: 불용어 처리

--- main/word_cloud.py
+++ main/word_cloud.py
@@ -10,22 +10,30 @@ import multiprocessing
 # nltk.download('averaged_perceptron_tagger')
 
 
-def test(comment):
+def wordDict(comment_list): # 수정본(한글 댓글 추출)
     okt = Okt()
-    language = isEnglishOrKorean(comment['comment']) # 댓글 언어 분석
-    comment = re.sub('[^A-Za-z0-9가-힣\s]', '', comment['comment'])
+    all_noun_list = []
+    stop_words_list = stopword()
+    
+    for comment in comment_list:
+        comment = re.sub('[^0-9가-힣\s]', '', comment['comment'])
 
-    # 언어에 따라 다른 형태소 분석기 사용
-    if language == "korean":
-        noun_list = [sent for sent in okt.nouns(comment) if len(sent) >=2] # 두 글자 이상의 명사 추출
-    else:
-        is_noun = lambda pos: pos[:2] =="NN"
-        tokenized = nltk.word_tokenize(comment)
-        noun_list = [sent for (sent, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+        noun_list = [sent for sent in okt.nouns(comment) if sent not in stop_words_list]
+        all_noun_list = all_noun_list + noun_list
 
-    return noun_list
+    word_dict = wordCount(all_noun_list)
+    return word_dict
 
-# def wordDict(comment_list):
+def stopword(): # 불용어 처리
+    korean_stopwords = open('./main/korean_stopwords.txt', 'r', encoding='utf-8')
+    stop_words_list = []
+    for line in korean_stopwords.readlines():
+        stop_words_list.append(line.rstrip()) #공백 제거
+
+    korean_stopwords.close()
+    return stop_words_list
+
+# def wordDict(comment_list): # 프로세싱 테스트
 #     all_noun_list = []
 #     pool = multiprocessing.Pool(processes= 5)
 #     all_noun_list = list(pool.map(test, comment_list))
@@ -35,31 +43,31 @@ def test(comment):
 #     word_dict = wordCount(all_noun_list)
 #     return word_dict
 
-def wordDict(comment_list): #원본
-    okt = Okt()
-    all_noun_list = []
+# def wordDict(comment_list): #원본
+#     okt = Okt()
+#     all_noun_list = []
 
-    for comment in comment_list:
-        language = isEnglishOrKorean(comment['comment']) # 댓글 언어 분석
-        comment = re.sub('[^A-Za-z0-9가-힣\s]', '', comment['comment'])
+#     for comment in comment_list:
+#         language = isEnglishOrKorean(comment['comment']) # 댓글 언어 분석
+#         comment = re.sub('[^A-Za-z0-9가-힣\s]', '', comment['comment'])
 
-        # 언어에 따라 다른 형태소 분석기 사용
-        if language == "korean":
-            noun_list = [sent for sent in okt.nouns(comment) if len(sent) >=2] # 두 글자 이상의 명사 추출
-        else:
-            is_noun = lambda pos: pos[:2] =="NN"
-            tokenized = nltk.word_tokenize(comment)
-            noun_list = [sent for (sent, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+#         # 언어에 따라 다른 형태소 분석기 사용
+#         if language == "korean":
+#             noun_list = [sent for sent in okt.nouns(comment) if len(sent) >=2] # 두 글자 이상의 명사 추출
+#         else:
+#             is_noun = lambda pos: pos[:2] =="NN"
+#             tokenized = nltk.word_tokenize(comment)
+#             noun_list = [sent for (sent, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
 
-        all_noun_list = all_noun_list + noun_list
+#         all_noun_list = all_noun_list + noun_list
 
-    word_dict = wordCount(all_noun_list)
-    return word_dict
+#     word_dict = wordCount(all_noun_list)
+#     return word_dict
 
-def isEnglishOrKorean(comment): #한/영 댓글 구분
-    k_count = len(re.sub('[^가-힣]', '', comment))
-    e_count = len(re.sub('[^-a-zA-Z]', '', comment))
-    return "korean" if k_count>e_count else "english"
+# def isEnglishOrKorean(comment): #한/영 댓글 구분
+#     k_count = len(re.sub('[^가-힣]', '', comment))
+#     e_count = len(re.sub('[^-a-zA-Z]', '', comment))
+#     return "korean" if k_count>e_count else "english"
 
 def wordCount(all_noun_list):
     word_count = {}
